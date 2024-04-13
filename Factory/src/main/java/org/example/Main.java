8f480c9040ead3140f066f02250f5602a91e4c4e
kamalkumar6785 <162416431+kamalkumar6785@users.noreply.github.com>
2024-04-14 00:53:35 +0530

    Update Main.java

--- Factory/src/main/java/org/example/Main.java
+++ Factory/src/main/java/org/example/Main.java
@@ -9,7 +9,6 @@ public class Main {
         Ticket premiumEconomyTicket = TicketFactory.createTicket(TicketFactory.TicketType.PREMIUM_ECONOMY,"New Delhi", "Dubai", 10000.0);
         Ticket businessTicket = TicketFactory.createTicket(TicketFactory.TicketType.BUSINESS, "Chennai","Kolkata", 7000.0);
         Ticket firstClassTicket = TicketFactory.createTicket(TicketFactory.TicketType.FIRST_CLASS, "Chennai","Ahmedabad", 4500.0);
-        Ticket Random = TicketFactory.createTicket(TicketFactory.TicketType.FIRST_CLASS, "XYZ","ABC", 4500.0);
 
 
         // call the display method for each ticket
